30ad54db1e0710bf379439cfe71e793d168c2333
JuanMedina05 <juanperezmedina2005@gmail.com>
2025-02-05 09:12:39 +0100

    Tablero y TableroTest solución de problemas

--- src/clases/Tablero.java
+++ src/clases/Tablero.java
@@ -37,9 +37,18 @@ public class Tablero {
         return this.flota;
     }
     public char getEstado(int f, int c){
+
         if(this.flota.getBarco(f,c)==null){
             return '.';
         }else {
+            int aux = this.flota.getBarco(f, c).getEstado(f, c);
+            switch (aux){
+                case 0 : return 'B';
+                case 1 : return '*';
+                case 2 : return 'x';
+                default : return 0;
+            }
+            /*
             if (this.flota.getBarco(f, c).getEstado(f, c) == 2) {
                 return 'x';
             } else if (this.flota.getBarco(f, c).getEstado(f, c) == 1) {
@@ -49,15 +58,20 @@ public class Tablero {
             }else{
                 return '0';
             }
+            */
         }
     }
     public void dibujar(boolean mostrarBarcos){
-        for(int i = 0;i<this.totalFilas;i++){
-            for(int j = 0;j<this.totalColumnas;j++){
-                if(mostrarBarcos){
-                    System.out.print(getEstado(i, j));
+        for(int i = 1;i<this.totalFilas+1;i++){
+            for(int j = 1;j<this.totalColumnas+1;j++){
+                if(!mostrarBarcos){
+                    if(getEstado(i,j)=='B'){
+                        System.out.print(".");
+                    }else{
+                        System.out.print(getEstado(i,j));
+                    }
                 }else{
-                    System.out.print(".");
+                    System.out.print(getEstado(i, j));
                 }
             }
             System.out.println("");
--- src/clases/test/TableroTest.java
+++ src/clases/test/TableroTest.java
@@ -15,12 +15,16 @@ public class TableroTest {
         t.getFlota().añadirBarco(new Barco(1,2,true,3));
         assertEquals('.',t.getEstado(2,1));
         assertEquals('B',t.getEstado(2,2));
-        t.getFlota().getBarco(2,2).recibirDisparo(2,2);
+        t.getFlota().recibirDisparo(2,2);
         assertEquals('*',t.getEstado(2,2));
-        t.getFlota().getBarco(1,2).recibirDisparo(1,2);
-        t.getFlota().getBarco(3,2).recibirDisparo(3,2);
+        t.getFlota().recibirDisparo(1,2);
+        t.getFlota().recibirDisparo(3,2);
         assertEquals('x',t.getEstado(2,2));
+        t.getFlota().añadirBarco(new Barco(5,2,false,4));
+        t.getFlota().añadirBarco(new Barco(6,7,true,4));
+        t.getFlota().recibirDisparo(8,7);
         t.dibujar(true);
+        System.out.println("");
         t.dibujar(false);
     }
 
